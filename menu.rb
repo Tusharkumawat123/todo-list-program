493f94015bc55628965c19a005a1fb8da7c7cbbb
tushar <tusharkumawat1912@gmail.com>
2021-10-18 11:46:41 +0530

    change menu file

---
+++ menu.rb
@@ -0,0 +1,34 @@
+class Calculator
+	def sum( num1,num2 )
+		@n1=num1
+		@n2=num2
+		@sum= @n1+@n2
+	end
+
+	def diffrance (num1,num2)
+		@n1=num1
+		@n2=num2
+		@diffrance= @n1-@n2
+	end
+
+	def multiply(num1,num2)
+		@n1=num1
+		@n2=num2
+		@multiply=num1*num2
+	end
+
+	def division (num1,num2)
+		@n1=num1
+		@n2=num2
+		@division=num1/num2
+	end
+end
+
+cal=Calculator.new
+
+print "Enter the first number:"
+num1=gets.chomp.to_i
+print "Enter the second number:"
+num2=gets.chomp.to_i
+
+puts "Enter the operation\ntype 'add' for Addition\ntype 'subtract' for Diffrance\ntype 'multiply' for multiply\ntype 'division' for division 
