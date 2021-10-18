3c0178314d462a60af64b0c803716756710c6099
tushar <tusharkumawat1912@gmail.com>
2021-10-18 13:41:39 +0530

    add new power programe

---
+++ power.rb
@@ -0,0 +1,18 @@
+#Programe is power of the number
+#create a method 
+def pow( a, b )
+	power = 1
+	for i in 1..b 
+		power = power * a
+	end
+	return power
+end
+
+#Take Base value and Exponent value from user
+print " Enter the base value :"
+Base = gets.chomp.to_i
+print "Enter the exponent value:"
+Expo = gets.chomp.to_i
+
+#Calling the Method
+puts "The power of the numbers is:#{pow( Base, Expo )}"
